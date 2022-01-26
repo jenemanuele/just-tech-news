ea015e30a49c93bd72c23283c519da185b1460cf
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-26 16:56:35 -0500

    added comment section

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-    attributes: [
+        order: [[ 'created_at', 'DESC']],
+        attributes: [
         'id',
         'post_url',
         'title',
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']],
     include: [
+        /// include comment model here
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
         {
         model: User,
         attributes: ['username']
