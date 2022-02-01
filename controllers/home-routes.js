2681b8a30fecaa1a793043393b6342992d303bd6
Jennifer Emanuele <jemanuele@gmail.com>
2022-02-01 12:26:24 -0500

    updated home route/ handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,17 +3,39 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https: //handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
-    });
+    Post.findAll({
+        attributes: [
+            'id', 
+            'post_url',
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*)FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attribues: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            //pass a single post object into the homepage template  
+        console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage',  {posts });
+        })  
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
